d743e517f1685fce235c9f06915ea2eeacc991ca
premthanachot <72982215+premthanachot@users.noreply.github.com>
2022-01-17 18:34:05 +0700

    commit

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,36 @@
+require("../models/database");
+const Category = require("../models/Category");
 
 /**
- * GET/ 
+ * GET/
  * Homepage
  */
-exports.homepage = async(req, res) => {
-    res.render('index');
-}
+exports.homepage = async (req, res) => {
+  try {
+
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render("index", {title: 'speed food', categories});
+  }catch (error){
+      res.satus(500).send({message: error.message || "Error Occured"});
+  }
+};
+
+// async function insertDymmyCategoryData() {
+//   try {
+//     await Category.insertMany(
+//         [
+//             { name: "Burgers", image: ".png" },
+//             { name: "Pizzas", image: ".png" },
+//             { name: "Appetizers", image: ".png" },
+//             { name: "Drinks", image: ".png" },
+//             { name: "Sweet", image: ".png" },
+//           ]
+//     );
+//   } catch (error) {
+//     console.log("error", +error);
+//   }
+// }
+
+// insertDymmyCategoryData();
